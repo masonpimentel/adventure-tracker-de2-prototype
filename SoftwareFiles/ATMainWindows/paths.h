c35262121cca9a4c9a1e0587b6452b729e9a5609
shawg <geoffshawbc@gmail.com>
2016-02-23 13:52:50 -0800

    Realtime path drawing

--- SoftwareFiles/ATMainWindows/paths.h
+++ SoftwareFiles/ATMainWindows/paths.h
@@ -8,10 +8,40 @@
 #ifndef PATHS_H_
 #define PATHS_H_
 
+#include "Fill.h"
+
+/* Struct for holding points to draw realtime */
+
+typedef struct {
+	float latitude;
+	float longitude;
+	XYPixel pixel;
+} RTPoint;
+
+typedef struct {
+	float delta_long;
+	float delta_lat;
+	float min_long;
+	float min_lat;
+	float max_long;
+	float max_lat;
+	int pixels_per_division_x;
+	int pixels_per_division_y;
+} path_info;
+
+#define MAX_REAL_POINTS 	512
+
+
+
+
+
+
+
 /* top level path drawing function for displaying logged trip */
 /* log is the number of the trip that we want to display */
 int DrawPath(int log);
 float ExtractAltitude(char* gpsdat);
+void updateRealTimePath(int latitude, int longitude);
 
 
 #endif /* PATHS_H_ */
